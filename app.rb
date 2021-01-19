14591242d1780db1b053ea503de75077e186d9fa
Jerome Wanliss <j.wanliss@yahoo.co.uk>
2021-01-19 15:41:03 +0000

    homepage done, now signup

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
+require 'erb'
 
 class MakersBnB < Sinatra::Base
-    get '/Spaces' do
+    
+    get '/' do
+        erb :index
+      end
+
+    get 'signup' do
+        erb :signup
+    end
+    
+    get '/spaces' do
         "Hello Spaces!"
     end
+    
     run! if app_file == $0
+
 end
