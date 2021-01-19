64fafd4b200bdbc7b6521c7102862b0cc73a780f
Tuyet Le <toowee.le@gmail.com>
2021-01-19 22:42:27 +0000

    update homepage feature test to pass it

--- spec/features/viewing_listing_spec.rb
+++ spec/features/viewing_listing_spec.rb
@@ -1,12 +1,3 @@
-
-feature 'view homepage' do
-    scenario 'checks user can see homepage' do
-        visit '/' 
-
-        expect(page).to have_content("homepage")
-    end
-end
-
 feature 'viewing listing' do
   scenario 'user can see multiple lisings on MakersBnB' do
     visit('/')
