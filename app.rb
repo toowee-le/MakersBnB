a0e6df93b643dd7fb3803eb06ac295b4a0488d80
rashidat <rasidatb@gmail.com>
2021-01-19 11:33:04 +0000

    list a new space

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
-    get '/Spaces' do
+ get '/' do
+    "Homepage"
+    erb :index
+ end
 
-       list_of_spaces = {:name => "the turtle suite", :description => "the most dreamiest beach house with
-        Sea views"}
-        erb :listings
-    end
+
+get '/spaces' do
+
+    @list_of_spaces = ["The Turtle Suite"]
+
+erb :listings
+end
     run! if app_file == $0
 end
--- spec/features/viewing_listing_spec.rb
+++ spec/features/viewing_listing_spec.rb
@@ -1,10 +1,19 @@
+
+feature 'view homepage' do
+    scenario 'checks user can see homepage' do
+        visit '/' 
+
+        expect(page).to have_content("homepage")
+    end
+end
+
+
+
 feature 'viewing listing' do
     scenario 'user can see multiple lisings on MakersBnB' do
         visit '/Spaces' 
 
         expect(page).to have_content("the turtle suite")
-        expect(page).to have_content("the most dreamiest beach house with
-            Sea views")
     end
 end
 
